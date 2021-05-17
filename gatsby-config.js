12c68eacb359baa0dc8f52dabb15898225ed5a65
Erpriliano Abbas <erpriliano@gmail.com>
2021-05-17 22:55:35 +0700

    Add analytics tool

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,20 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-2DT292LDRM", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
